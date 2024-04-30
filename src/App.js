f4978d46b052eef69833764032406ccccc8c47d1
tahoang7 <102772329+tahoang7@users.noreply.github.com>
2024-04-29 22:06:45 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Yo it's tony
         </a>
       </header>
     </div>
