df5fbc14c9344285bd8caf19e97c61aea5948e16
tahoang7 <102772329+tahoang7@users.noreply.github.com>
2024-04-29 22:13:31 -0700

    version 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Yo it's tony
+          Tony's portfolio
         </a>
       </header>
     </div>
